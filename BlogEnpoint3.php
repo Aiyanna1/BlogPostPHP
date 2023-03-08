d7011b2ce9c67873cabd83b13ecb93374399060b
Aiyanna1 <121715056+Aiyanna1@users.noreply.github.com>
2023-03-08 04:24:10 +0200

    changes made to if else in endpoint3

--- BlogEnpoint3.php
+++ BlogEnpoint3.php
@@ -1,43 +1,30 @@
 <?php
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') { //checks if the method used is the Post method
+  $commentId = $_POST['id'] ?? '';  // This will get the ID of the coment from the request parameter
   
-  
-  $commentId = $_POST['id'] ?? '';
-  
- 
-  if (!empty($commentId)) {
+  if (!empty($commentId)) {  //sees if comment ID is a valid ID
+    $filePath = 'filestore/text.json' . $commentId . '.json'; // this creates a path to the file that contains the comment
     
-    $filePath = 'filestore/text.json' . $commentId . '.json';
-    
-    
-    if (file_exists($filePath)) {
-      
-      
-      unlink($filePath);
-      
-      
-      $metadata = json_decode(file_get_contents('filestore/metadata.json'), true) ?? [];
-      
-      if (($key = array_search($filePath, $metadata)) !== false) {
+    if (file_exists($filePath)) {  // if the file exists
+      unlink($filePath);  // if exists this will delete the file
+      $metadata = json_decode(file_get_contents('filestore/metadata.json'), true) ?? [];//This will get the metadata
+       if (($key = array_search($filePath, $metadata)) !== false) { //This will remove the comment file name from the metadata
         unset($metadata[$key]);
       } 
-      file_put_contents('filestore/metadata.json', json_encode($metadata));
+      file_put_contents('filestore/metadata.json', json_encode($metadata)); //This will save the new metadata file
       http_response_code(200);
-      echo json_encode(array('message' => 'Comment deleted successfully.'));
+      echo json_encode(array('message' => 'Comment deleted successfully.'));//and will then send a success message
       
     } else { 
       http_response_code(404);
-      echo json_encode(array('message' => 'Comment not found.'));
+      echo json_encode(array('message' => 'Comment not found.')); //if above not available or the comment ID doesnt exist, it will give this error message
     }
   } else {
     http_response_code(400);
-    echo json_encode(array('message' => 'Comment ID is required.'));
+    echo json_encode(array('message' => 'Comment ID is required.'));//If the person didnt enter the comment ID this error message will appear
   }
-} else {
-  http_response_code(405);
-  echo json_encode(array('message' => 'Method not allowed.'));
 }
 
 ?>
